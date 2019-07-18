ef5e9fb73eddccee2b8658399687973857ebb371
NikitaUshenko <nikita-ushenko@mail.ru>
2019-07-18 16:10:51 +0300

    Updates

---
+++ precache-manifest.9dcf5d92ba6fa96e1eb6b998033e1ff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40ed1650d7321eaf6f73d8a2ebfc14a",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "508df76ae1229c49d016",
+    "url": "/react_dynamic-list-of-todos/static/css/main.f164f98b.chunk.css"
+  },
+  {
+    "revision": "c389f7078157a01ed7a8",
+    "url": "/react_dynamic-list-of-todos/static/js/2.d21bc449.chunk.js"
+  },
+  {
+    "revision": "508df76ae1229c49d016",
+    "url": "/react_dynamic-list-of-todos/static/js/main.5c5369dd.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
